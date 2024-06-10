e1be12b69ebecad1edcd8d2929c47466e7b5e6aa
saifec1127 <saifec1127@gmail.com>
2024-06-10 22:27:04 +0530

    Add CI/CD pipeline with GitHub Actions by resolving error again

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders the header', () => {
+test('renders React project header', () => {
   render(<App />);
   const headerElement = screen.getByText(/React project/i);
   expect(headerElement).toBeInTheDocument();
