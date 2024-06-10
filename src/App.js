a87fd87499dd78616bf6baece597f9ac2e64ecae
saifec1127 <saifec1127@gmail.com>
2024-06-10 21:39:19 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React app with data feching from API
         </p>
         <a
           className="App-link"
