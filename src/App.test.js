375ed28c5e13831a2bf7d775c2cc8b559f6267f5
saifec1127 <saifec1127@gmail.com>
2024-06-11 11:09:56 +0530

    clean react app

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders React project header', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/React project/i);
-  expect(headerElement).toBeInTheDocument();
-});
