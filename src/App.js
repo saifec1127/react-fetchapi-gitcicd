4b4cc76c9d29dc253c93c112a5f4fc346dea5af9
saifec1127 <saifec1127@gmail.com>
2024-06-12 21:10:50 +0530

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ const App =()  => {
     <div className="App">
       <header className="App-header">
       <h1>React project</h1>
+      <p>
+          Now Application is build and deploy by gitHub Actions.
+        </p>
         <p>
           React app with data feching from API and controlling version with git
         </p>
